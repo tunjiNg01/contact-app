d5a52c3b21f4e74f2819a6d387744ef8b677e524
tunjiNg01 <nahtlosng@gmail.com>
2023-04-21 16:13:04 +0100

    init

---
+++ contact-application/contact-application/contact_applicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  contact_applicationApp.swift
+//  contact-application
+//
+//  Created by MACBOOK PRO on 21/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct contact_applicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
