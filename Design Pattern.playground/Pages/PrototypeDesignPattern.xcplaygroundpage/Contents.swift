edfefcccb997b38a139795b69aef1a9d94fc7d7a
tunjiNg01 <nahtlosng@gmail.com>
2023-08-02 12:49:45 +0100

    prototype design pattern

--- Design Pattern.playground/Pages/PrototypeDesignPattern.xcplaygroundpage/Contents.swift
+++ Design Pattern.playground/Pages/PrototypeDesignPattern.xcplaygroundpage/Contents.swift
@@ -1,31 +1,48 @@
 //: [Previous](@previous)
 
 import Foundation
+ 
 
-class Person: NSCopying {
-   
-    var name: String
-    var age: Int
 
-    init(name: String, age: Int) {
-        self.name = name
-        self.age = age
+
+
+
+class GameEngine: NSCopying {
+    var level: Int;
+    
+     init(level: Int) {
+        self.level = level
+       
+     }
+    
+    
+   public func copy(with zone: NSZone? = nil) -> Any {
+        return GameEngine(level: level);
     }
     
-    func copy(with zone: NSZone? = nil) -> Any {
-        return Person(name: name, age: age)
+    public func start(){
+        switch level {
+        case 1:
+            print("starting level one...")
+        case 2:
+            print("starting level two...")
+        case 3:
+            print("starting level three...")
+        default:
+            print("currently in default mode")
+        }
     }
-   
 }
 
 
-let prototypePerson = Person(name: "John Doe", age: 30)
 
-let newPerson = prototypePerson.copy() as! Person
 
-newPerson.name = "Luke Thomas"
-newPerson.age = 25
+var game = GameEngine(level: 1)
 
-print(newPerson.name)
-print(newPerson.age)
 
+
+var gameCopy = game.copy() as! GameEngine
+
+
+gameCopy.level = 1
+gameCopy.start()
